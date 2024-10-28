1f6ac3be79679c9c673f3a94a965fd5760e4c1b7
devizer <devizer@github.io>
2024-10-28 19:44:02 +0200

    TODO: Duplicates

---
+++ Universe.GenericTreeTable.Tests/SimpleTreeTest.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Universe.GenericTreeTable.Tests
+{
+	public class SimpleTreeTest
+	{
+
+		[Test]
+		public void TestUnique()
+		{
+			var plainRows = TestSeeder.Seed(3).ToArray();
+			var plainRowsAsDebug = plainRows.Select(x => $"[{string.Join(",", x.Key.ToArray())}]").ToArray();
+			var builder = new TreeTableBuilder<string, TestNodeData>(TestTreeConfiguration.Instance);
+			var consoleTable = builder.Build(plainRows);
+			Console.WriteLine(consoleTable);
+		}
+
+		[Test]
+		public void TestDuplicateLastAndFirst()
+		{
+			var plainRows = TestSeeder.Seed(3).ToList();
+			if (plainRows.Count > 2)
+			{
+				plainRows = plainRows.Concat(new[] { plainRows.First(), plainRows.Last() }).ToList();
+			}
+
+			var plainRowsAsDebug = plainRows.Select(x => $"[{string.Join(",", x.Key.ToArray())}]").ToArray();
+			var builder = new TreeTableBuilder<string, TestNodeData>(TestTreeConfiguration.Instance);
+			var consoleTable = builder.Build(plainRows);
+			Console.WriteLine(consoleTable);
+		}
+
+	}
+}
